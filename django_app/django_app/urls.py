18ef2716e2007763d5324a2d3c81956811fed848
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2017-11-04 23:12:36 -0500

    Url file added

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-import django.conf.urls import include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
