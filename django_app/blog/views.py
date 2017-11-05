b24bb1eded5da52dd1d7d317a783281f005881d1
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2017-11-05 00:37:04 -0500

    Index modified

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Hello world!! this is a django site!!!</h2>")
+    return HttpResponse("<h2>Hello world!!! this is a django site!!!</h2>")
