34bfec06388019237de804b1c49bb10b4a97ae63
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2017-11-04 22:59:05 -0500

    Url file added

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -20,7 +20,7 @@ import django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-url(r'^polls/', include('blog.urls')),
+    url(r'^polls/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
 
 ]
