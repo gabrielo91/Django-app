1cd48181c1686502a774376af0adced0ce8ac362
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2017-11-06 13:14:13 -0500

    file updated

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    print ('hola hola hola')
+    print ('hola hola Daniela')
     print ('hola hola hola')
     print ('hola hola hola')
     print ('hola hola hola')
