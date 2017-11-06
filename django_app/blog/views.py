3e4b469da64d7e4aa800f2e725093adc7f922781
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2017-11-06 13:12:05 -0500

    new changes

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Hello world!!! this is a django site!!!</h2>")
+    print ('hola hola hola')
+    print ('hola hola hola')
+    print ('hola hola hola')
+    print ('hola hola hola')
+    print ('hola hola hola')
+    print ('hola hola hola')
+
+    return HttpResponse("<h2>Hello world!!! this is a django site. Hola Danny!!!</h2>")
